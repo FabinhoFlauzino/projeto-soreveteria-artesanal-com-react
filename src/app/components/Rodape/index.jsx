9f69baa0c69fa92b3f4c91aa6d484a7364ffa0be
unknown <fabioflauzino@hotmail.com>
2024-05-11 00:08:08 -0300

    desktop finalizado

--- src/app/components/Rodape/index.jsx
+++ src/app/components/Rodape/index.jsx
@@ -1,21 +1,30 @@
 import Image from "next/image";
+import styles from "./Rodape.module.css"
+import Logo from "../../../../public/logo.png"
 
-export default function Rodape () {
+export default function Rodape() {
   return (
-    <footer>
-      <Image src={""} title="Logo" alt="Logo"/>
-      <div>
+    <footer className={styles.container_rodape}>
+      <div className={styles.container_informacoes}>
+        <Image src={Logo} className={styles.img} alt="logomarca" />
         <div>
-          <h3>Endereço</h3>
-          <p>Av. Qualquer Uma, 200</p>
-          <p>Marília-SP 17507-000</p>
+          <h3>ENDEREÇO</h3>
+          <p>Av. Bernardino de Campos, 98</p>
+          <p>São Paulo, SP 12345-678</p>
         </div>
-
         <div>
-          <h3>Contao</h3>
-          <p>Av. Qualquer Uma, 200</p>
-          <p>Marília-SP 17507-000</p>
+          <h3>CONTATO</h3>
+          <p>info@meusite.com</p>
+          <p>Tel: (11) 3456-7890</p>
         </div>
+        <div>
+          <h3>HORÁRIOS</h3>
+          <p>ABERTO TODOS OS DIAS</p>
+          <p>10:00 - 22:00</p>
+        </div>
+      </div>
+      <div className={styles.container_direito_autoral}>
+        <p>Gelateria. Orgulhosamente desenvolvido por "Fabinho Flauzino"</p>
       </div>
     </footer>
   )
--- src/app/components/Topo/index.jsx
+++ src/app/components/Topo/index.jsx
@@ -1,13 +1,14 @@
 import Image from "next/image";
 import Link from "next/link";
 import styles from "./Topo.module.css";
+import Logo from "../../../../public/logo.png"
 
 export default function Topo() {
   return (
     <header className={styles.header_container}>
       <div className="container">
         <div className={styles.header_content}>
-          <Image src={""} title="Logo" alt="Logo" />
+        <Image className={styles.img} src={Logo} alt="logomarca"/>
           <nav>
             <Link href="/">Home</Link>
             <Link href="/sabores">Sabores</Link>
