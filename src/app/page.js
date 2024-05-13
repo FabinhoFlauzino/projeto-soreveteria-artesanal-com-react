36ae3b8f5553d6a6d2b8af4864c5070b204b5d92
unknown <fabioflauzino@hotmail.com>
2024-05-12 22:36:59 -0300

    finalizado projeto

--- src/app/page.js
+++ src/app/page.js
@@ -36,7 +36,7 @@ export default function Home() {
           <Image src={Imagem3} alt="Imagem" title="imagem" />
         </div>
         <div className={styles.content_info}>
-          <h2>NSOBRE NÓS</h2>
+          <h2>SOBRE NÓS</h2>
           <span>Alegria em cada casquinha!</span>
           <p>Venha tomar o melhor sorvete da região aqui com a gente! Nós estamos há anos no mercado produzindo o que tem de melhor para o 	nosso cliente e você não pode ficar fora dessa. Venha nos fazer uma visita e aproveite o melhor atendimento e o melhor sorvete da cidade.</p>
         </div>
